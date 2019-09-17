3c094992ee951c76bd1da96efe7d3ac5887d2f43
zc310 <liushiyan@msn.com>
2019-09-17 18:09:38 +0800

    新增 测试代码

---
+++ test/cache.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	"cache"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Cache(t *testing.T, c cache.Cache) {
+	var err error
+	key := []byte("key1")
+	value := []byte("1234567890")
+
+	err = c.SetTimeout(key, value, 0)
+	assert.Equal(t, err, nil)
+
+	b, ok := c.Get(key)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, b, value)
+
+	key = []byte("key3")
+	err = c.SetTimeout(key, value, time.Minute)
+	assert.Equal(t, err, nil)
+
+	b, ok = c.Get(key)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, b, value)
+
+	k := []byte("abc")
+	v := []byte("0123456789")
+	err = c.SetTimeout(k, v, time.Hour*24)
+	assert.Equal(t, err, nil)
+
+	b, ok = c.Get(k)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, b, v)
+
+	b, ok = c.GetRange(k, 6, 9)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, b, []byte("678"))
+
+	err = c.SetTimeout(k, []byte("abc"), time.Hour*24)
+	assert.Equal(t, err, nil)
+
+	b, ok = c.Get(k)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, b, []byte("abc"))
+
+	err = c.Delete(k)
+	assert.Equal(t, err, nil)
+
+	b, ok = c.Get(k)
+	assert.Equal(t, ok, false)
+}
